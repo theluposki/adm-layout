df6e4b4a3cb7d7012fce34a04c39a8f51a6b7e7e
theluposki <theluposki@gmail.com>
2026-03-11 02:05:26 -0300

    s

--- src/pages/config.vue
+++ src/pages/config.vue
@@ -20,9 +20,20 @@ const info = () => {
 <template>
   <div class="page">
     <h1>configurações</h1>
-    <div class="btn" @click="save">Salvar</div>
-    <div class="btn" @click="error">Erro</div>
-    <div class="btn" @click="info">info</div>
-
+    <div class="container">
+      <div class="btn btn-full" @click="save">Salvar</div>
+      <div class="btn btn-full" @click="error">Erro</div>
+      <div class="btn btn-full" @click="info">Info</div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  max-width: 280px;
+  margin: 24px auto;
+  gap: var(--p);
+}
+</style>
